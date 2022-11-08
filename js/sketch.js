36875404e9a8001725bc591ef26258287bc4c70f
ozannicholls <116882906+ozannicholls@users.noreply.github.com>
2022-11-08 10:00:38 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,20 @@
+var firstName = "Ozan";
+var string = "hello";
+var int = 500;
+
+
+var eclipseD = 500;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    console.log(firstName);
 }
 
 function draw() {
     background(220);
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(400, 400, mouseX-mouseY);
+    console.log(mouseX, mouseY);
 }
